20d5dc42bf168334340a10000bcec0926a4052b4
erik.forsberg <devnull@localhost>
2006-12-09 20:28:05 +0000

    * Implemented simple mapping between sf.net status/resolution and   new tracker status/resolution:
         Keep old values, except that all issues with Deleted status now get
         the Closed status.
    
      See http://psf.upfronthosting.co.za/roundup/meta/issue55 for
      details.
    
      Added new status/resolution values to initial_data.py
    
    * Adapted templates to make default issue listings show only open and
      pending issues.

--- initial_data.py
+++ initial_data.py
@@ -36,15 +36,22 @@ priority.create(name='normal', order='4')
 priority.create(name='low', order='5')
 
 status = db.getclass('status')
-status.create(name='new', order='1')
-status.create(name='open', order='2')
+status.create(name='open', order='1')
+status.create(name='closed', order='2')
 status.create(name='pending', description='user feedback required', order='3')
-status.create(name='closed', order='4')
 
 resolution = db.getclass('resolution')
-resolution.create(name='fixed', order='1')
-resolution.create(name='invalid', order='2')
-resolution.create(name='duplicate', order='3')
+resolution.create(name='accepted', order='1')
+resolution.create(name='duplicate', order='2')
+resolution.create(name='fixed', order='3')
+resolution.create(name='invalid', order='4')
+resolution.create(name='later', order='5')
+resolution.create(name='out of date', order='6')
+resolution.create(name='postponed', order='7')
+resolution.create(name='rejected', order='8')
+resolution.create(name='remind', order='9')
+resolution.create(name='wont fix', order='10')
+resolution.create(name='works for me', order='11')
 
 #
 # create the two default users
