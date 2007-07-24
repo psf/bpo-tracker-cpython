25902412d34d3147015d23e9e16f6e61675e620d
erik.forsberg <devnull@localhost>
2007-07-24 19:15:39 +0000

    Moved configuration (spambayes server uri and cutoff values) into detectors/config.ini.

--- detectors/spamcheck.py
+++ detectors/spamcheck.py
@@ -9,31 +9,25 @@ valid submissions at the expense of manual checks to correct spammy items
 which snuck by the screen.
 """
 
-HOST = "www.webfast.com"
-PORT = "80"
-PATH = "/sbrpc"
-
-# These must match the {ham,spam}_cutoff setting in the SpamBayes server
-# config.
-HAM_CUTOFF = 0.2
-SPAM_CUTOFF = 0.85
-
 import xmlrpclib
 import socket
 
 from roundup.exceptions import Reject
 
-def check_spam(_database, _klass, _nodeid, newvalues):
+def check_spam(_db, _klass, _nodeid, newvalues):
     """Auditor to score a website submission."""
 
-    uri = "http://%s:%s%s" % (HOST, PORT, PATH)
-    server = xmlrpclib.ServerProxy(uri, verbose=False)
+    spambayes_uri = _db.config.detectors['SPAMBAYES_URI']
+    spam_cutoff = float(_db.config.detectors['SPAMBAYES_SPAM_CUTOFF'])
+
+
+    server = xmlrpclib.ServerProxy(spambayes_uri, verbose=False)
     try:
         prob = server.score(newvalues, [], {})
-    except (socket.error, xmlrpclib.Error):
+    except (socket.error, xmlrpclib.Error), e:
         pass
     else:
-        if prob >= SPAM_CUTOFF:
+        if prob >= spam_cutoff:
             raise Reject("Looks like spam to me - prob=%.3f" % prob)
 
 def init(database):
