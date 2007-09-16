cf91a6f4c60c1348e4b30e73fe7cfa101d35b6bc
erik.forsberg <devnull@localhost>
2007-09-16 12:31:17 +0000

    Automatically link to svn.python.org for revision ID's.
    Resolves most of http://psf.upfronthosting.co.za/roundup/meta/issue154.

--- extensions/local_replace.py
+++ extensions/local_replace.py
@@ -2,7 +2,11 @@ import re
 
 substitutions = [ (re.compile('\#(?P<ws>\s*)(?P<id>\d+)'),
                    "<a href='issue\g<id>'>#\g<ws>\g<id></a>" ),
-                  ]
+                  (re.compile('(?P<prews>\s+)revision(?P<ws>\s*)(?P<revision>\d+)'),
+                   "\g<prews><a href='http://svn.python.org/view?rev=\g<revision>&view=rev'>revision\g<ws>\g<revision></a>"),
+                  (re.compile('(?P<prews>\s+)(?P<revstr>r|r\s+)(?P<revision>\d+)'),
+                   "\g<prews><a href='http://svn.python.org/view?rev=\g<revision>&view=rev'>\g<revstr>\g<revision></a>"),
+                   ]
 
 def localReplace(message):
 
@@ -15,4 +19,12 @@ def localReplace(message):
     
 def init(instance):
     instance.registerUtil('localReplace', localReplace)
+    
+
+if "__main__" == __name__:
+    print " revision 222", localReplace(" revision 222")
+    print " wordthatendswithr 222", localReplace(" wordthatendswithr 222")
+    print " r222", localReplace(" r222")
+    print " r 222", localReplace(" r 222")
+    print " #555", localReplace(" #555")
     
