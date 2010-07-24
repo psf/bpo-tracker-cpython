055ef25e90a40289200e029e5819456d59a18f30
ezio.melotti <devnull@localhost>
2010-07-24 12:29:51 +0000

    #285: add a tooltip that shows status and title of linked issues on the tracker. Also avoid linking to unexisting issues.

--- extensions/local_replace.py
+++ extensions/local_replace.py
@@ -1,34 +1,86 @@
 import re
+import cgi
+from roundup import hyperdb
+from roundup.cgi.templating import register_propclass, StringHTMLProperty
+
 
 substitutions = [
-    #  #1234, # 1234
-    (re.compile(r'\#(?P<ws>\s*)(?P<id>1?\d{4,6})\b'),
-     r'<a href="issue\g<id>">#\g<ws>\g<id></a>'),
     #  r12345, r 12345, rev12345, rev 12345, revision12345, revision 12345
     (re.compile(r'\b(?<!/)(?P<revstr>r(ev(ision)?)?\s*)(?P<revision>\d+)'),
-     r'<a href="http://svn.python.org/view?rev=\g<revision>&view=rev">\g<revstr>\g<revision></a>'),
+     r'<a href="http://svn.python.org/view?rev=\g<revision>'
+     r'&view=rev">\g<revstr>\g<revision></a>'),
+
     # Lib/somefile.py, Modules/somemodule.c, Doc/somedocfile.rst, ...
-    (re.compile(r'(?P<sep>(?<!\w/)|(?<!\w)/)(?P<path>(?:Demo|Doc|Grammar|Include|Lib|Mac|Misc|'
-                r'Modules|Parser|PC|PCbuild|Python|RISCOS|Tools|Objects)/[-.a-zA-Z0-9_/]+[a-zA-Z0-9]/?)'),
-     r'<a href="http://svn.python.org/view/python/trunk/\g<path>">\g<sep>\g<path></a>'),
+    (re.compile(r'(?P<sep>(?<!\w/)|(?<!\w)/)(?P<path>(?:Demo|Doc|Grammar|'
+                r'Include|Lib|Mac|Misc|Modules|Parser|PC|PCbuild|Python|'
+                'RISCOS|Tools|Objects)/[-.a-zA-Z0-9_/]+[a-zA-Z0-9]/?)'),
+     r'<a href="http://svn.python.org/view/python/'
+     r'trunk/\g<path>">\g<sep>\g<path></a>'), # XXX trunk is dead
 ]
 
-def localReplace(message):
-    """
-    Turn the following strings in HTML links:
-    #1234, # 1234  ->  <a href="issue1234">#1234</a>
-    r12345, r 12345, rev12345, rev 12345, revision12345, revision 12345
-        ->  <a href="http://svn.python.org/view?rev=12345&view=rev">r12345</a>
-    Lib/somefile.py, Modules/somemodule.c, Doc/somedocfile.rst, ...
-        ->  <a href="http://svn.python.org/view/python/trunk/Dir/filename.ext">Dir/filename.ext</a>
-    """
-    for cre, replacement in substitutions:
-        message = cre.sub(replacement, message)
 
-    return message
+# if the issue number is too big the db will explode -- limit it to 7 digits
+issue_re = re.compile(r'(?P<text>(\#|issue)\s*(?P<id>1?\d{1,6}))\b', re.I)
 
+
+class PyDevStringHTMLProperty(StringHTMLProperty):
+    def _hyper_repl(self, match):
+        """
+        Override the original method and change it to still linkify URLs and
+        emails but avoid linkification of issues and other items
+        (except messages and files).
+        """
+        if match.group('url'):
+            return self._hyper_repl_url(match, '<a href="%s">%s</a>%s')
+        elif match.group('email'):
+            return self._hyper_repl_email(match, '<a href="mailto:%s">%s</a>')
+        elif (len(match.group('id')) < 10 and
+              match.group('class') and
+              match.group('class').lower() in ('msg', 'file')):
+            # linkify msgs but not issues and other things
+            return self._hyper_repl_item(match,
+                '<a href="%(cls)s%(id)s">%(item)s</a>')
+        else:
+            # just return the matched text
+            return match.group(0)
+
+    def pydev_hyperlinked(self):
+        """Create python-dev-specific links."""
+        # first do the normal linkification (without linkify the issues)
+        message =  self.plain(hyperlink=1)
+        # then add links for revision numbers and paths
+        for cre, replacement in substitutions:
+            message = cre.sub(replacement, message)
+        # finally add links for issues
+        message = issue_re.sub(self._linkify_issue, message)
+        return message
+
+    def _linkify_issue(self, match):
+        """Turn an issue (e.g. 'issue 1234' or '#1234') to an HTML link"""
+        template = ('<a class="%(status)s" title="[%(status)s] %(title)s" '
+                    'href="issue%(issue_id)s">%(text)s</a>')
+        issue_id = match.group('id')
+        text = match.group('text')
+        cl = self._db.issue
+        # check if the issue exists
+        if not cl.hasnode(issue_id):
+            return text
+        # get the title
+        title = cgi.escape(cl.get(issue_id, 'title').replace('"', "'"))
+        status_id = cl.get(issue_id, 'status')
+        # get the name of the status
+        if status_id is not None:
+            status = self._db.status.get(status_id, 'name')
+        else:
+            status = 'none'
+        return template % dict(issue_id=issue_id, title=title,
+                               status=status, text=text)
+
+
+# XXX there should be only one regex for this
 noise_change = re.compile('(nosy_count|message_count)\: \d+\.0 -> \d+\.0')
 noise_init = re.compile('(nosy_count|message_count)\: \d+\.0')
+# XXX this shouldn't be a regex
 br = re.compile('<td><br />')
 
 def clean_count(history):
@@ -38,73 +90,5 @@ def clean_count(history):
     return history
 
 def init(instance):
-    instance.registerUtil('localReplace', localReplace)
+    register_propclass(hyperdb.String, PyDevStringHTMLProperty)
     instance.registerUtil('clean_count', clean_count)
-
-if "__main__" == __name__:
-    import unittest
-
-    class TestLocalReplace(unittest.TestCase):
-        def test_localReplace(self):
-            test_strings = [
-                ## r12345, r 12345, rev12345, rev 12345, revision12345, revision 12345
-                ('r222 ',
-                 '<a href="http://svn.python.org/view?rev=222&view=rev">r222</a> '),
-                (' r222 ',
-                 ' <a href="http://svn.python.org/view?rev=222&view=rev">r222</a> '),
-                (' r 222 ',
-                 ' <a href="http://svn.python.org/view?rev=222&view=rev">r 222</a> '),
-                (' rev222 ',
-                 ' <a href="http://svn.python.org/view?rev=222&view=rev">rev222</a> '),
-                (' rev  222 ',
-                 ' <a href="http://svn.python.org/view?rev=222&view=rev">rev  222</a> '),
-                (' revision222 ',
-                 ' <a href="http://svn.python.org/view?rev=222&view=rev">revision222</a> '),
-                (' revision 222 ',
-                 ' <a href="http://svn.python.org/view?rev=222&view=rev">revision 222</a> '),
-                ('wordthatendswithr 222',
-                 'wordthatendswithr 222'),
-                ('http://svn.python.org/view/python/tags/r265 ',
-                 'http://svn.python.org/view/python/tags/r265 '),
-
-                ##  #1234, # 1234
-                ## the lowest issue id is #1000, the highest is #1779871
-                (' #1 ', ' #1 '),
-                (' #10 ', ' #10 '),
-                (' #999 ', ' #999 '),
-                (' # 999 ', ' # 999 '),
-                ('#1000 ', '<a href="issue1000">#1000</a> '),
-                (' #1000 ', ' <a href="issue1000">#1000</a> '),
-                (' # 1000 ', ' <a href="issue1000"># 1000</a> '),
-                (' #5555 ', ' <a href="issue5555">#5555</a> '),
-                (' # 5555 ', ' <a href="issue5555"># 5555</a> '),
-                (' #555555 ', ' <a href="issue555555">#555555</a> '),
-                (' # 555555 ', ' <a href="issue555555"># 555555</a> '),
-                (' #1999999 ', ' <a href="issue1999999">#1999999</a> '),
-                (' # 1999999 ', ' <a href="issue1999999"># 1999999</a> '),
-                (' #2000000 ', ' #2000000 '),
-                (' # 2000000 ', ' # 2000000 '),
-
-                ## Lib/somefile.py, Modules/somemodule.c, Doc/somedocfile.rst, ...
-                ('Lib/cgi.py',
-                 '<a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">Lib/cgi.py</a>'),
-                ('/Lib/cgi.py',
-                 '<a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">/Lib/cgi.py</a>'),
-                ('the bug is in Lib/cgi.py.',
-                 'the bug is in <a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">Lib/cgi.py</a>.'),
-                ('the bug is in /Lib/cgi.py.',
-                 'the bug is in <a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">/Lib/cgi.py</a>.'),
-                ('(Lib/cgi.py)',
-                 '(<a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">Lib/cgi.py</a>)'),
-                ('(/Lib/cgi.py)',
-                 '(<a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">/Lib/cgi.py</a>)'),
-                ('http://svn.python.org/view/python/tags/r265/Lib/cgi.py',
-                 'http://svn.python.org/view/python/tags/r265/Lib/cgi.py'),
-                ('http://svn.python.org/view/python/tags/r265/Lib/cgi.py?view=markup',
-                 'http://svn.python.org/view/python/tags/r265/Lib/cgi.py?view=markup'),
-            ]
-            for text, expected_result in test_strings:
-                self.assertEqual(expected_result, localReplace(text))
-
-    # run the tests when executed directly
-    unittest.main()
---
+++ extensions/test/test_local_replace.py
@@ -0,0 +1,150 @@
+import sys
+import unittest
+import os.path
+
+if len(sys.argv) != 2:
+    sys.exit('Error: You have to provide the path of Roundup in order to run '
+             'the tests (e.g. /opt/tracker-roundup/lib/python2.6/site-packages/).')
+# add to sys.path the dir where roundup is installed (local_replace will try
+# to import it)
+sys.path.append(sys.argv.pop())
+
+dirs = os.path.dirname(os.path.abspath(__file__)).split(os.path.sep)
+# add the dir where local_replace is (i.e. one level up)
+sys.path.append(os.path.sep.join(dirs[:-1]))
+# add the dir where the roundup tests are
+sys.path.append(os.path.sep.join(dirs[:-4] + ['roundup-src', 'test']))
+
+
+from local_replace import PyDevStringHTMLProperty
+from test_templating import MockDatabase, TemplatingTestCase
+
+
+class MockDBItem(object):
+    def __init__(self, values):
+        # *values* is a list of ids that are supposed to exist in the db
+        # (note that there's no difference here between issues, msg, etc.)
+        self.ids = map(str, values)
+
+    def hasnode(self, id):
+        return id in self.ids
+
+    def get(self, id, value):
+        if value == 'title':
+            return 'Mock title'
+        if value == 'status':
+            return 1
+        if value == 'name':
+            return 'open'
+
+class PyDevMockDatabase(MockDatabase):
+    def __init__(self, values):
+        self.issue = self.msg = self.status = MockDBItem(values)
+    def getclass(self, cls):
+        return self.issue
+
+
+test_strings = [
+    ## r12345, r 12345, rev12345, rev 12345, revision12345, revision 12345
+    ('r222 ',
+     '<a href="http://svn.python.org/view?rev=222&view=rev">r222</a> '),
+    (' r222 ',
+     ' <a href="http://svn.python.org/view?rev=222&view=rev">r222</a> '),
+    (' r 222 ',
+     ' <a href="http://svn.python.org/view?rev=222&view=rev">r 222</a> '),
+    (' rev222 ',
+     ' <a href="http://svn.python.org/view?rev=222&view=rev">rev222</a> '),
+    (' rev  222 ',
+     ' <a href="http://svn.python.org/view?rev=222&view=rev">rev  222</a> '),
+    (' revision222 ',
+     ' <a href="http://svn.python.org/view?rev=222&view=rev">revision222</a> '),
+    (' revision 222 ',
+     ' <a href="http://svn.python.org/view?rev=222&view=rev">revision 222</a> '),
+    ('wordthatendswithr 222',
+     'wordthatendswithr 222'),
+
+
+    ##  #1234, # 1234
+    ## the lowest issue id on bugs.python.org is #1000, the highest is #1779871
+    (' #1 ', ' #1 '),
+    (' #10 ', ' #10 '),
+    (' #999 ', ' #999 '),
+    (' # 999 ', ' # 999 '),
+    ('#1000 ',
+     '<a class="open" title="[open] Mock title" href="issue1000">#1000</a> '),
+    (' #1000 ',
+     ' <a class="open" title="[open] Mock title" href="issue1000">#1000</a> '),
+    (' # 1000 ',
+     ' <a class="open" title="[open] Mock title" href="issue1000"># 1000</a> '),
+    (' #5555 ',
+     ' <a class="open" title="[open] Mock title" href="issue5555">#5555</a> '),
+    (' # 5555 ',
+     ' <a class="open" title="[open] Mock title" href="issue5555"># 5555</a> '),
+    (' #555555 ',
+     ' <a class="open" title="[open] Mock title" href="issue555555">#555555</a> '),
+    (' # 555555 ',
+     ' <a class="open" title="[open] Mock title" href="issue555555"># 555555</a> '),
+    (' #1999999 ',
+     ' <a class="open" title="[open] Mock title" href="issue1999999">#1999999</a> '),
+    (' # 1999999 ',
+     ' <a class="open" title="[open] Mock title" href="issue1999999"># 1999999</a> '),
+    (' #1020 ', ' #1020 '),
+    (' #2000000 ', ' #2000000 '),
+    (' # 2000000 ', ' # 2000000 '),
+    (' #1234567890123  ', ' #1234567890123  '),
+
+    ## Lib/somefile.py, Modules/somemodule.c, Doc/somedocfile.rst, ...
+    ('Lib/cgi.py',
+     '<a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">Lib/cgi.py</a>'),
+    ('/Lib/cgi.py',
+     '<a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">/Lib/cgi.py</a>'),
+    ('see Lib/cgi.py.',
+     'see <a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">Lib/cgi.py</a>.'),
+    ('see /Lib/cgi.py.',
+     'see <a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">/Lib/cgi.py</a>.'),
+    ('(Lib/cgi.py)',
+     '(<a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">Lib/cgi.py</a>)'),
+    ('(/Lib/cgi.py)',
+     '(<a href="http://svn.python.org/view/python/trunk/Lib/cgi.py">/Lib/cgi.py</a>)'),
+
+    ## URLs
+    ('http://svn.python.org/view/python/tags/r265 ',
+     ('<a href="http://svn.python.org/view/python/tags/r265">'
+      'http://svn.python.org/view/python/tags/r265</a> ')),
+    ('http://svn.python.org/view/python/tags/r265/Lib/cgi.py',
+     ('<a href="http://svn.python.org/view/python/tags/r265/Lib/cgi.py">'
+      'http://svn.python.org/view/python/tags/r265/Lib/cgi.py</a>')),
+    ('http://svn.python.org/view/python/tags/r265/Lib/cgi.py?view=markup',
+     ('<a href="http://svn.python.org/view/python/tags/r265/Lib/cgi.py?view=markup">'
+      'http://svn.python.org/view/python/tags/r265/Lib/cgi.py?view=markup</a>')),
+
+    ## emails
+    ('fixed@europython.eu',
+     '<a href="mailto:fixed@europython.eu">fixed@europython.eu</a>'),
+    ('<fixed@europython.eu>',
+     '&lt;<a href="mailto:fixed@europython.eu">fixed@europython.eu</a>&gt;'),
+
+    ## items
+    ('see msg1000.', 'see <a href="msg1000">msg1000</a>.'),
+    ('see msg 1000.', 'see <a href="msg1000">msg 1000</a>.'),
+    ('see file1000.', 'see <a href="file1000">file1000</a>.'),
+    ('see file 1000.', 'see <a href="file1000">file 1000</a>.'),
+    # only msgs and files are linkified
+    ('see version1000.', 'see version1000.'),
+    ('see version 1000.', 'see version 1000.'),
+]
+
+
+class TestPyDevStringHTMLProperty(TemplatingTestCase):
+    def test_replacement(self):
+        # create a db with a few issue/msg/file ids
+        self.client.db = self._db = PyDevMockDatabase(
+                [1000, 5555, 555555, 1999999, 2000000, 1234567890123])
+        self.client.db.security.hasPermission = lambda *args, **kw: True
+        for text, expected_result in test_strings:
+            p = PyDevStringHTMLProperty(self.client, 'test', '1',
+                                        None, 'test', text)
+            self.assertEqual(expected_result, p.pydev_hyperlinked())
+
+# run the tests
+unittest.main()
