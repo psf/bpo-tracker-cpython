94bd2a4752e3310e85dbe30e2a9b3324b3b57f65
martin.v.loewis <devnull@localhost>
2010-08-22 06:25:55 +0000

    Suppress display of user history to anybody but the user.

---
+++ extensions/suppress_history.py
@@ -0,0 +1,13 @@
+def is_history_ok(request):
+    user = request.client.userid
+    db = request.client.db
+    classname = request.classname
+    nodeid = request.nodeid
+    # restrict display of user history to user itself only
+    if classname == 'user':
+        return user == nodeid
+    # currently not used
+    return True
+
+def init(instance):
+    instance.registerUtil('is_history_ok', is_history_ok)
