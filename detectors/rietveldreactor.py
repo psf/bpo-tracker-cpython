2285e149dadfbf742395f0a0d76d8e7b81ca5c7e
martin.v.loewis <devnull@localhost>
2010-10-02 07:46:34 +0000

    Import existing users and issues into rietveld.

--- detectors/rietveldreactor.py
+++ detectors/rietveldreactor.py
@@ -6,6 +6,8 @@ empty_list = base64.encodestring(cPickle.dumps([]))
 def create_django_user(db, cl, nodeid, oldvalues):
     username = cl.get(nodeid, 'username')
     email = cl.get(nodeid, 'address')
+    if email is None:
+        email = ''
     c = db.cursor
     # django.contrib.auth.models.UNUSABLE_PASSWORD=='!'
     c.execute("insert into auth_user(id, username, email, password, first_name, last_name, "
