5db44c882d234806b226f994f5b4bc23edc1643f
martin.v.loewis <devnull@localhost>
2010-10-02 18:18:43 +0000

    Wrap Issue and File.

--- rietveld/roundup_helper/models.py
+++ rietveld/roundup_helper/models.py
@@ -13,3 +13,19 @@ class User(models.Model):
     _address = models.CharField()
     class Meta:
         db_table = '_user'
+
+class File(models.Model):
+    _creator = models.IntegerField()
+    _creation = models.DateTimeField()
+    _branch = models.CharField()
+    _revision = models.CharField()
+    _patchset = models.CharField()
+    class Meta:
+        db_table = '_file'
+
+class Issue(models.Model):
+    _creator = models.IntegerField()
+    _creation = models.DateTimeField()
+    _status = models.IntegerField()
+    class Meta:
+        db_table = '_issue'
