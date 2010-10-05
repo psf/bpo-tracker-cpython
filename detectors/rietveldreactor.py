66f5e22d6eef664c7abbda4368605ba76aedf46c
martin.v.loewis <devnull@localhost>
2010-10-05 13:32:06 +0000

    Create issue only when first patch is added.

--- detectors/rietveldreactor.py
+++ detectors/rietveldreactor.py
@@ -15,17 +15,7 @@ def create_django_user(db, cl, nodeid, oldvalues):
               "values(%s, %s, %s, '!', '', '', false, true, false, now(), now())",
               (nodeid, username, email))
 
-def create_rietveld_issue(db, cl, nodeid, oldvalues):
-    subject = cl.get(nodeid, 'title')
-    c = db.cursor
-    # find owner id: same in django as in rietveld
-    owner_id = cl.get(nodeid, 'creator')
-    c.execute("insert into codereview_issue(id, subject, owner_id, reviewers, cc, created, modified, closed, private, n_comments) "
-              "values(%s, %s, %s, %s, %s, now(), now(), false, false, 0)",
-              (nodeid, subject, owner_id, empty_list, empty_list))
-
 def init(db):
     db.user.react('create', create_django_user)
     # XXX react to email changes, roles
-    db.issue.react('create', create_rietveld_issue)
-    # XXX reacto to subject, closed changes
+    # XXX react to subject, closed changes on issues
