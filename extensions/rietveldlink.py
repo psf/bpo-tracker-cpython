e8fd56ae8793ead0a6c4c8a4ebb4918b61e85095
martin.v.loewis <devnull@localhost>
2010-11-06 11:25:10 +0000

    Fix typo

--- extensions/rietveldlink.py
+++ extensions/rietveldlink.py
@@ -1,5 +1,5 @@
 def rietveldlink(request, issueid, fileid):
-    patchset = request.client.db.file.get(fileid, 'patchset'):
+    patchset = request.client.db.file.get(fileid, 'patchset')
     if patchset and patchset != 'n/a':
         return '/review/%s/show' % issueid
     return ""
