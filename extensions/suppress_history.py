dc4fd9557d6c586e7c9141ef5b2bc72e7062900c
martin.v.loewis <devnull@localhost>
2011-03-21 21:34:05 +0000

    Allow coordinators to see the user's history.

--- extensions/suppress_history.py
+++ extensions/suppress_history.py
@@ -5,7 +5,7 @@ def is_history_ok(request):
     nodeid = request.nodeid
     # restrict display of user history to user itself only
     if classname == 'user':
-        return user == nodeid
+        return user == nodeid or 'Coordinator' in db.user.get(user, 'roles')
     # currently not used
     return True
 
