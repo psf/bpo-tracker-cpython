a86016a36a0c63f4cd4eaf04c61b793487405b35
ezio.melotti <devnull@localhost>
2011-07-22 22:34:53 +0000

    #267: remove the remove button from the issue page, move it to the msg/file page, and add a button to add back removed messages/files.

--- extensions/pydevutils.py
+++ extensions/pydevutils.py
@@ -14,6 +14,29 @@ def is_coordinator(request):
     db = request.client.db
     return 'Coordinator' in db.user.get(user, 'roles')
 
+
+def issueid_and_action_from_class(cls):
+    """
+    Return the id of the issue where the msg/file is/was linked
+    and if the last "linking action" was 'link' or 'unlink'.
+    """
+    last_action = ''
+    for entry in cls._klass.history(cls._nodeid):
+        if 'unlink' in entry:
+            last_unlink = entry
+            last_action = 'unlink'
+        elif 'link' in entry:
+            last_entry = entry
+            last_action = 'link'
+    if last_action in ('link', 'unlink'):
+        # the msg has been unlinked and not linked back
+        # the link looks like: ('16', <Date 2011-07-22.05:14:12.342>, '4',
+        #                       'link', ('issue', '1', 'messages'))
+        return last_entry[4][1], last_action
+    return None, None
+
 def init(instance):
     instance.registerUtil('is_history_ok', is_history_ok)
     instance.registerUtil('is_coordinator', is_coordinator)
+    instance.registerUtil('issueid_and_action_from_class',
+                          issueid_and_action_from_class)
