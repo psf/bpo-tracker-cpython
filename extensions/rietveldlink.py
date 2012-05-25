3b0cf60309061fea3aa277648318730164c7dd91
martin.v.loewis <devnull@localhost>
2012-05-25 17:51:27 +0000

    Deep-link to patch set.

--- extensions/rietveldlink.py
+++ extensions/rietveldlink.py
@@ -1,7 +1,7 @@
 def rietveldlink(request, issueid, fileid):
     patchset = request.client.db.file.get(fileid, 'patchset')
     if patchset and patchset != 'n/a':
-        return '/review/%s/show' % issueid
+        return '/review/%s/#ps%s' % (issueid, patchset)
     return ""
 
 def init(instance):
