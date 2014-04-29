c91acd6c5f68b0efc54593fee216a8d40c5b256f
Ezio Melotti <ezio.melotti@gmail.com>
2014-04-29 19:18:30 +0300

    Fix search by giving access to the "id" field too.

--- schema.py
+++ schema.py
@@ -330,7 +330,7 @@ db.security.addPermissionToRole('Developer', 'SB: May Classify')
 
 # Allow Users and Developers to view most user properties.
 p =  db.security.addPermission(name='View', klass='user',
-   properties=('username', 'address', 'realname', 'phone',
+   properties=('id', 'username', 'address', 'realname', 'phone',
          'organisation', 'alternate_addresses', 'timezone'))
 db.security.addPermissionToRole('User', p)
 db.security.addPermissionToRole('Developer', p)
